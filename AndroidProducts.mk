284466356921db66b1e9db559f5fff8756244831
GPJ <grzegorz.jaworski1@gmail.com>
2022-06-05 11:56:43 +0200

    Bringup crDroid

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,8 +15,8 @@
 #
 
 PRODUCT_MAKEFILES += \
-    $(LOCAL_DIR)/evolution_judypn.mk
+    $(LOCAL_DIR)/lineage_judypn.mk
 
 COMMON_LUNCH_CHOICES := \
-    evolution_judypn-userdebug \
-    evolution_judypn-eng
+    lineage_judypn-userdebug \
+    lineage_judypn-eng
