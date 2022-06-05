0833b3fdd8e4aa20f8dfb1426aea2585aa15ee57
GPJ <grzegorz.jaworski1@gmail.com>
2022-06-05 12:05:41 +0200

    It's crDroid time, let's do this!

--- lineage_judypn.mk
+++ lineage_judypn.mk
@@ -28,20 +28,28 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 # Inherit from judypn device
 $(call inherit-product, device/lge/judypn/device.mk)
 
-# Inherit some common Evolution X stuff.
-$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+# Inherit some common crDroid stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 TARGET_SUPPORTS_QUICK_TAP := true
-EVO_BUILD_TYPE := UNOFFICIAL
+TARGET_SUPPORT_FACE_UNLOCK := true
+BUILD_HOSTNAME := crDroid-for U
+BUILD_USERNAME := gregoorr
+
+# Maintainter
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.maintainer.name=gregoorr
 
 # Boot animation
 TARGET_BOOT_ANIMATION_RES := 1440
+TARGET_SCREEN_HEIGHT := 3120
+TARGET_SCREEN_WIDTH := 1440
 
 # Overlays (inherit after vendor/cm to ensure we override it)
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 # Device identifiers
 
-PRODUCT_NAME := evolution_judypn
+PRODUCT_NAME := lineage_judypn
 PRODUCT_DEVICE := judypn
 PRODUCT_BRAND := lge
 PRODUCT_MANUFACTURER := LGE
@@ -52,6 +60,3 @@ PRODUCT_GMS_CLIENTID_BASE := android-om-lg
 
 TARGET_VENDOR_PRODUCT_NAME := judypn_lao_eea
 TARGET_VENDOR_DEVICE_NAME := judypn
-
-
-
